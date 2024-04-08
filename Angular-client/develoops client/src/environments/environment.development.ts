285d5dd40572d92e39bc86b922a16914fed9b0ee
DvoriProgrammer <dvori456@gmail.com>
2024-04-08 22:42:09 +0300

    fix environment

--- Angular-client/develoops client/src/environments/environment.development.ts
+++ Angular-client/develoops client/src/environments/environment.development.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = { production: false,
+    apiUrl: 'https://api.openai.com/v1/chat/completions', 
+    apiKey:"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 "};
