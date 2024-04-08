04bfe77370696b19d94072982730a67074d17e9f
DvoriProgrammer <dvori456@gmail.com>
2024-04-08 22:58:27 +0300

    hide e

--- Angular-client/develoops client/src/environments/environment.development.ts
+++ Angular-client/develoops client/src/environments/environment.development.ts
@@ -2,7 +2,5 @@
 
 
 export const environment = { 
-    production: false,
-    apiUrl: 'https://api.openai.com/v1/chat/completions', 
-    apiKey:"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 "
+  
 };
